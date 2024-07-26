0d0da91ada5e57817c69e39cf9d371a3ac34a3ce
Tatianasvet <168006070+Tatianasvet@users.noreply.github.com>
2024-07-26 19:01:45 +0500

    multiprocessing

---
+++ module_10_5.py
@@ -0,0 +1,42 @@
+from multiprocessing import Process, Pool
+from threading import Thread
+
+
+class WarehouseManager:
+    def __init__(self):
+        self.data = {}
+
+    def process_request(self, request):
+        name = request[0]
+        command = request[1]
+        amount = request[2]
+        if command == 'receipt':
+            if name in self.data.keys():
+                self.data[name] += amount
+            else:
+                self.data[name] = amount
+        elif command == 'shipment':
+            if name in self.data.keys() and self.data[name] >= amount:
+                self.data[name] -= amount
+
+    def run(self, requests):
+        actions = []
+        for request in requests:
+            proc = Process(target=self.process_request, args=(request,))
+            actions.append(proc)
+            proc.start()
+        for proc in actions:
+            proc.join()
+
+
+if __name__ == '__main__':
+    manager = WarehouseManager()
+    requests = [
+        ("product1", "receipt", 100),
+        ("product2", "receipt", 150),
+        ("product1", "shipment", 30),
+        ("product3", "receipt", 200),
+        ("product2", "shipment", 50)
+    ]
+    manager.run(requests)
+    print(manager.data)
